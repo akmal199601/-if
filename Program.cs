f93f8ef8528be817f0b50f264d5cf125232c2a6a
akmal199601 <akmal199601@mail.ru>
2020-03-26 15:20:06 +0300

    дз по if

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Задание 2  ////
+            // int a = 9;
+            // int b = 8;
+            // if(a==b)a=0;b=0;
+            // if (a<b)a=b;
+            // if (a>b)b=a;
+            // System.Console.WriteLine(a);
+            // System.Console.WriteLine(b);
+
+
+        //     /// Задание 3////
+        //     string action;
+        //     Console.WriteLine("Введите первое число");
+        //    double a = double.Parse(Console.ReadLine());
+
+        //     Console.WriteLine("Введите второе число");
+        //    double b = double.Parse(Console.ReadLine());
+
+        //     Console.WriteLine("Выберите операцию:'+''-''*''/'");
+        //     action = Console.ReadLine();
+
+        //     switch (action)
+        //     {
+        //         case "+":
+        //             Console.WriteLine(a + b);
+        //             break;
+        //         case "-":
+        //             Console.WriteLine(a - b);
+        //             break;
+        //         case "*":
+        //             Console.WriteLine(a * b);
+        //             break;
+        //         case "/":
+        //             if (b==0)
+                    
+        //                 Console.WriteLine("Ошибка");
+                    
+        //             else
+                    
+        //                 Console.WriteLine(a / b);
+                    
+        //             Console.WriteLine(a / b);
+        //             break;
+                
+
+        //     }
+
+        //     Console.ReadLine();
+
+           //// Задание////
+
+            // int a = 3; 
+            // switch (a)
+            // {
+            //     if(a >= 0 && a < 14)
+            //     {
+            //     System.Console.WriteLine("0 - 14");
+            //     }
+            //     else if (a >= 15 && a < 35)
+            //      {
+            //     System.Console.WriteLine("15 - 35");
+            //      }
+            //    else if (a >= 36 && a < 50)
+            //     System.Console.WriteLine("36 - 50");
+            //      {
+            //    else  if (a >= 50 && a < 100 )
+            //     System.Console.WriteLine("50 - 100");
+            //      }
+            //     else
+            //     {
+            //        System.Console.WriteLine("Не входящий ни в один промежуток");         
+            //     }
+        }
+    }
+}
